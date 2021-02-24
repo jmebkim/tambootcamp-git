6a8e74c4ded2a40c810bc1ac3e145dfaf3dd9f1c
Jaimie Kim <jaimiek@jaimiek-a03.vmware.com>
2021-02-24 16:13:52 -0700

    checkin some deletes

--- hello-world.go
+++ hello-world.go
@@ -1,13 +1,9 @@
-# add a new comment
-# add a second comment
+# add a new comment # add a second comment
 # add a third comment
 # add a fourth comment
 # file:  hello-world.go
 # description:  my very first go program
-# jaimie line of comments
 # Feb24 comment from local
-# Feb24 diff test comment
-# more worthless comments
 
 package main
 import "fmt"
